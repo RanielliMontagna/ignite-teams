c78a00f0d76ea93bcf2cf0c0d10c1a08e18e9930
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-08-25 13:55:01 -0300

    feat: :sparkles: implementa funcionalidades de players, melhora usabilidade e finaliza projeto

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,57 +1,80 @@
-import { useCallback, useState } from "react";
-import { FlatList } from "react-native";
-import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { useState, useCallback } from 'react';
+import { Alert, FlatList } from 'react-native';
 
-import { groupsGetAll } from "@storage/group/groupsGetAll";
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
-import { GroupCard } from "@components/GroupCard";
-import { Header } from "@components/Header";
-import { Highlight } from "@components/Highlight";
-import { ListEmpty } from "@components/ListEmpty";
-import { Button } from "@components/Button";
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
-import { Container } from "./styles";
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+import { Container } from './styles';
+import { Loading } from '@components/Loading';
 
 export function Groups() {
-  const navigation = useNavigation();
-
+  const [isLoading, setIsLoading] = useState(true);
   const [groups, setGroups] = useState<string[]>([]);
 
+  const navigation = useNavigation();
+
   function handleNewGroup() {
-    navigation.navigate("new");
+    navigation.navigate('new');
   }
 
   async function fetchGroups() {
     try {
+      setIsLoading(true);
       const data = await groupsGetAll();
-      setGroups(data);
+      setGroups(data)
     } catch (error) {
+      Alert.alert('Turmas', 'Não foi possível carregar as turmas');
       console.log(error);
-    }
+    } finally {
+      setIsLoading(false);
+    } 
   }
 
-  useFocusEffect(
-    useCallback(() => {
-      fetchGroups();
-    }, [])
-  );
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+  
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  },[]))
+  
 
   return (
     <Container>
       <Header />
-      <Highlight title="Turmas" subtitle="jogue com sua turma" />
-
-      <FlatList
-        data={groups}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => (
-          <ListEmpty message="Que tal cadastrar a primeira turma?" />
-        )}
+      <Highlight 
+        title="Turmas"
+        subtitle="jogue com sua turma"
       />
+      {
+        isLoading ? <Loading /> :
+          <FlatList 
+            data={groups}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+              <GroupCard 
+                title={item}
+                onPress={() => handleOpenGroup(item)}
+              />
+            )}
+            contentContainerStyle={groups.length === 0 && { flex: 1 }}
+            ListEmptyComponent={() => (
+              <ListEmpty message="Que tal cadastrar a primeira turma?" />
+            )}
+          />
+      }
 
-      <Button title="Criar nova turma" onPress={handleNewGroup} />
+      <Button 
+        title='Criar nova turma'
+        onPress={handleNewGroup}
+      />
     </Container>
   );
 }
--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,8 +1,8 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { AppError } from "@utils/AppError";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AppError } from '@utils/AppError';
 
-import { GROUP_COLLECTION } from "@storage/storageConfig";
-import { groupsGetAll } from "./groupsGetAll";
+import { groupsGetAll } from './groupsGetAll';
+import { GROUP_COLLECTION } from '@storage/storageConfig';
 
 export async function groupCreate(newGroup: string) {
   try {
@@ -10,13 +10,13 @@ export async function groupCreate(newGroup: string) {
 
     const groupAlreadyExists = storedGroups.includes(newGroup);
 
-    if (groupAlreadyExists) {
-      throw new AppError("Já existe um grupo cadastrado com esse nome.");
+    if(groupAlreadyExists) {
+      throw new AppError('Já existe um grupo cadastrado com esse nome.')
     }
 
-    const storage = JSON.stringify([...storedGroups, newGroup]);
-
+    const storage = JSON.stringify([...storedGroups, newGroup])
     await AsyncStorage.setItem(GROUP_COLLECTION, storage);
+    
   } catch (error) {
     throw error;
   }
