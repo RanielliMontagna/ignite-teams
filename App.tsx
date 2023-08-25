c987612c51112f6af843d6073551214c385a7bff
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-08-25 13:30:23 -0300

    feat: :sparkles: implementa navegação utilizando stack navigator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components/native'; 
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import theme from './src/theme';
+import theme from "./src/theme";
 
-import { Loading } from '@components/Loading';
-
-import { Players } from '@screens/Players';
+import { Loading } from "@components/Loading";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -14,12 +17,12 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <>
-        <StatusBar 
+        <StatusBar
           barStyle="light-content"
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <Players/> : <Loading/> }
+        {fontsLoaded ? <Routes /> : <Loading />}
       </>
     </ThemeProvider>
   );
